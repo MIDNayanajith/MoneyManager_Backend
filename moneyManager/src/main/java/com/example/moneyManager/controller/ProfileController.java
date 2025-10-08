c7a29a0d3dae7f9833aaef2ce883429419df68be
MIDNayanajith <isuranga_2020736@fot.ruh.ac.lk>
2025-10-08 23:03:57 +0530

    implement email service

--- moneyManager/src/main/java/com/example/moneyManager/controller/ProfileController.java
+++ moneyManager/src/main/java/com/example/moneyManager/controller/ProfileController.java
@@ -7,9 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,5 +19,16 @@ public class ProfileController {
     public ResponseEntity<ProfileDto> registerProfile(@RequestBody ProfileDto profileDto){
         ProfileDto registeredProfile = profileService.registerProfile(profileDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(registeredProfile);
+    }
+
+    @GetMapping("/activate")
+    public ResponseEntity<String>activateProfile(@RequestParam String token){
+        boolean isActivated = profileService.activateProfile(token);
+        if(isActivated){
+            return ResponseEntity.ok("Profile Activated Successfully!");
+        }
+        else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Activation token not found or already used!");
+        }
     }
 }
--- moneyManager/src/main/java/com/example/moneyManager/service/ProfileService.java
+++ moneyManager/src/main/java/com/example/moneyManager/service/ProfileService.java
@@ -13,11 +13,16 @@ import java.util.UUID;
 public class ProfileService {
 
     private final ProfileRepository profileRepository;
+    private final EmailService emailService;
 
     public ProfileDto registerProfile(ProfileDto profileDto){
         ProfileEntity newProfile = toEntity(profileDto);
         newProfile.setActivationToken(UUID.randomUUID().toString());
         newProfile = profileRepository.save(newProfile);
+        String activationLink = "http://localhost:8080/api/v1.0/activate?token="+newProfile.getActivationToken();
+        String subject = "Activate your Account";
+        String body = "Click on the following link to activate your account:"+activationLink;
+        emailService.sendEmail(newProfile.getEmail(),subject,body);
         return toDto(newProfile);
 
     }
@@ -44,5 +49,15 @@ public class ProfileService {
                 .createdAt(profileEntity.getCreatedAt())
                 .updatedAt(profileEntity.getUpdatedAt())
                 .build();
+    }
+
+    public boolean activateProfile(String activationToken){
+        return profileRepository.findByActivationToken(activationToken)
+                .map(profile -> {
+                    profile.setIsActive(true);
+                    profileRepository.save(profile);
+                    return true;
+                })
+                .orElse(false);
     }
 }
